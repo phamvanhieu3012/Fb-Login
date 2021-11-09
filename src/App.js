9af87bd864d764b2441de2b2059dc35dedad6d25
phamvanhieu3012 <phamvanhieu74123@gmail.com>
2021-11-10 00:08:19 +0700

    Code Login Fb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FacebookLogin from "react-facebook-login";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const responseFacebook = (response) => {
+    console.log(response);
+    setIsLoggedIn(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggedIn ? (
+        "Home"
+      ) : (
+        <FacebookLogin
+          appId="452317036044756"
+          autoLoad={true}
+          fields="name,email,picture"
+          // onClick={componentClicked}
+          callback={responseFacebook}
+        />
+      )}
     </div>
   );
 }
